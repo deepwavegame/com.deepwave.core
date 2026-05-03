cd581cfee3f9b952c9d5ee762c358321a807fb4e
deepwavegame <deepwavegame@gmail.com>
2026-05-03 20:48:29 +0700

    update: DynamicRangeAttribute

--- Assets/Deepwave/WaveCore/Editor/Utilities/PropertyUtility.cs
+++ Assets/Deepwave/WaveCore/Editor/Utilities/PropertyUtility.cs
@@ -13,19 +13,47 @@ namespace Deepwave.Core.Editor
         /// Gets the dynamic maximum limit based on a DynamicRangeAttribute's DynamicMaxList property.
         /// If no dynamic max is configured, the default max is returned.
         /// </summary>
-        public static float GetDynamicMax(SerializedProperty property, float defaultMax, DynamicRangeAttribute attr)
+        public static float GetDynamicMax(
+            SerializedProperty property,
+            float defaultMax,
+            DynamicRangeAttribute attr
+        )
         {
             if (attr == null || string.IsNullOrEmpty(attr.DynamicMaxList))
                 return defaultMax;
 
             int lastDotIndex = property.propertyPath.LastIndexOf('.');
-            string parentPath = lastDotIndex == -1 ? string.Empty : property.propertyPath[..lastDotIndex] + ".";
+            string parentPath =
+                lastDotIndex == -1 ? string.Empty : property.propertyPath[..lastDotIndex] + ".";
             string listPath = $"{parentPath}{attr.DynamicMaxList}";
 
             var listProp = property.serializedObject.FindProperty(listPath);
-            if (listProp != null && listProp.isArray)
+            if (listProp != null)
             {
-                return Mathf.Max(attr.Min, listProp.arraySize - 1);
+                float val;
+                if (listProp.isArray)
+                {
+                    val = listProp.arraySize;
+                }
+                else if (listProp.propertyType == SerializedPropertyType.Integer)
+                {
+                    val = listProp.intValue;
+                }
+                else if (listProp.propertyType == SerializedPropertyType.Float)
+                {
+                    val = listProp.floatValue;
+                }
+                else
+                {
+                    return defaultMax;
+                }
+
+                if (attr.Squared)
+                {
+                    val *= val;
+                }
+
+                return Mathf.Max(attr.Min, val - 1);
             }
 
             return defaultMax;
